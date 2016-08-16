20efa6949882aa97af6a86cdb10f829385627085
damcapn <calihistyle101@gmail.com>
2016-08-16 12:48:23 -0600

    added an image to image file.
    added headutils file and all the apropriate links.

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8">
     <title> Start Up Hub </title>
+    <?php require_once("headutils.php");?>
+    <link rel="stylesheet" href="lib/css/style.css">
 
-  <link rel="stylesheet" href="lib/css/style.css">
-  
   </head>
   <body>
-
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
   </body>
 </html>
